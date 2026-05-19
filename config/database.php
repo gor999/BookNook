b79f55324672960976461ddb26f0ceda1a9ec5fc
gor999 <gor.iskandaryan@gmail.com>
2026-05-19 13:27:29 +0400

    db connection change

--- config/database.php
+++ config/database.php
@@ -44,24 +44,13 @@ return [
         ],
 
         'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
+    'driver' => 'mysql',
+    'url' => env('DB_URL'),
+    'host' => env('DB_HOST', 'mysql-3619758c-gor-6b78.j.aivencloud.com'), // Տեղադրեք ձեր Aiven Host-ը
+    'port' => env('DB_PORT', '19820'), // Տեղադրեք ձեր Aiven Port-ը
+    'database' => env('DB_DATABASE', 'defaultdb'),
+    'username' => env('DB_USERNAME', 'avnadmin'),
+],
 
         'mariadb' => [
             'driver' => 'mariadb',
